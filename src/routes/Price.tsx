cb041df448ddc48d23a9001de93a9e5f16331cbd
HyungJin <hyungjin0710@gmail.com>
2021-12-12 04:28:32 +0900

    Implement the /:coinId/price tab

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,78 @@
-function Price() {
-  return <h1>Price</h1>;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface PriceProps {
+  coinId: string;
+}
+
+function Price({ coinId }: PriceProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading price..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[{ name: "sales", data: data?.map((price) => price.close) }]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              show: true,
+              curve: "smooth",
+              lineCap: "butt",
+              colors: undefined,
+              width: 2,
+              dashArray: 0,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#00a8ff"], stops: [0, 100] },
+            },
+            colors: ["#e84118"],
+            tooltip: {
+              y: { formatter: (value) => `${value.toFixed(2)}` },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Price;
